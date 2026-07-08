7a2e0b8fbd59400393f7147a0517f3b4ab38d65d
Danilchenko Mihail <mihaildanilchenko85@gmail.com>
2026-07-08 22:46:36 +0300

    correct algoritm new

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import NewPicture from "./jumper";
 
 const interval = 1000;
 const evend = 4;
-const lineOf = 5;
+const lineOf = 4;
 
 document.addEventListener("DOMContentLoaded", () => {
   const widget = new NewPicture(document.querySelectorAll(".cell"));
@@ -18,17 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
     liner.push(document.getElementById(`line${i + 1}`));
   }
 
-  function obrabotchik(event) {
-    console.log('зашел в обработчик', event)
-    if (event.target.tagName == "IMG") {
-      const finer = document.getElementsByClassName("fine")[0];
-      finer.textContent = Number(finer.textContent) + 1;
-      window.widget.addPicture();
-      const badler = document.getElementsByClassName("mistake")[0];
-      badler.textContent = 0;
-    }
-  }
-
   //стираем все!!!!!!!!!!!!!!
   function lastic() {
     document.getElementsByClassName("mistake")[0].textContent = 0;
@@ -41,24 +30,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const badler = document.getElementsByClassName("mistake")[0];
     badler.textContent = Number(badler.textContent) + 1;
     if (Number(badler.textContent) > lineOf) {
-      lastic();
-
-      const dialog = document.getElementById('myDialog')
-      dialog.showModal()
-      document.getElementById('closeDialog').addEventListener('click', ()=>{
-        dialog.close()
-      })
-      clearInterval(timer);
-      document.querySelectorAll('.cell')[widget.position].innerHTML = '' 
-
-      /*if (confirm("Вы проиграли...  Хотите начать заново?")) {
-        clearInterval(timer);
-        timer = setInterval(() => {
-          moved();
-        }, interval);
-      } else {
-               
-      } */      
+      const dialog = document.getElementById("myDialog");
+      dialog.showModal();
+      document.getElementById("closeDialog").addEventListener("click", () => {
+        document.querySelectorAll(".cell")[widget.position].innerHTML = "";
+        lastic();
+        dialog.close();
+      });
+      clearInterval(timer);      
     }
   }
 
@@ -68,22 +47,19 @@ document.addEventListener("DOMContentLoaded", () => {
         const finer = document.getElementsByClassName("fine")[0];
         finer.textContent = Number(finer.textContent) + 1;
 
-        clearInterval(timer)
+        clearInterval(timer);
         window.widget.addPicture();
         timer = setInterval(() => {
           moved();
-        }, interval)
-        
+        }, interval);
+
         const badler = document.getElementsByClassName("mistake")[0];
         badler.textContent = 0;
       }
-    })
+    });
   });
 
   timer = setInterval(() => {
     moved();
-  }, interval)
-  
-})
-
-  
+  }, interval);
+});
